7a34e237846b99ba84a328119015f3c94bd636fc
rgugs <rgugler@gmail.com>
2026-01-13 13:01:53 -0800

    Updated to Python 3.14 for t-strings capability

---
+++ src/main.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    # Import file with geopands
+    # Format into table with Waypoint name, lat, lon
+    # Inject into FPL and/or KML templates
+    # Write out file
+    pass
+
+
+if __name__ == "__main__":
+    main()
